0c396ad68660ce8d9d752675d436af9bf8da3bf4
cwood89 <cwood89@mac.com>
2020-05-26 14:30:14 -0400

    looped through accounts

--- Bank.java
+++ Bank.java
@@ -1,6 +1,5 @@
+import java.util.LinkedList;
 import java.util.List;
-
-import javax.swing.text.Utilities;
 
 public class Bank {
   public static void main(String[] args) {
@@ -16,10 +15,27 @@ public class Bank {
 
     // read csv files and create accounts based on that data
 
+    List<Account> accounts = new LinkedList<Account>();
+
     String file = "/home/chris/Documents/javaBankAccount/original.csv";
     List<String[]> newAccountHolders = utilities.CSV.read(file);
     for (String[] accountHolder : newAccountHolders) {
-
+      String name = accountHolder[0];
+      String ssn = accountHolder[1];
+      String accountType = accountHolder[2];
+      double deposit = Double.parseDouble(accountHolder[3]);
+      // System.out.println(name + " " + ssn + " " + accountType + " " + deposit);
+      if (accountType.equals("Checking")) {
+        accounts.add(new Checking(name, ssn, deposit));
+      } else if (accountType.equals("Savings")) {
+        accounts.add(new Savings(name, ssn, deposit));
+      } else {
+        System.out.println("Invalid account type.");
+      }
+      for (Account acc : accounts) {
+        acc.showInfo();
+        System.out.println("\n");
+      }
     }
 
   }
